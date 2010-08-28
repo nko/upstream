b6b6fc528431c8388e747fd26a99a2a0e8af5a27
Alexander Lang <alex@upstream-berlin.com>
2010-08-28 13:25:16 +0200

    get apartment location from google and store in document

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ require.paths.unshift('vendor/node-couchdb/lib');
 
 var express = require('express/index'),
     connect = require('connect/index'),
+    hl_http_client = require('./lib/highlevel_http_client'),
+    querystring = require('querystring'),
     sys     = require('sys');
 
 var app = module.exports = express.createServer();
+
+
 var couchdb = require('couchdb'), client, db;
+
 var couch_views = require('./lib/couch_views');
 var view_helpers = require('./lib/view_helpers');
 
@@ -42,6 +47,9 @@ app.configure('production', function(){
   google_maps_key = 'ABQIAAAASOw3kHJFc2xCpxnZ-dtD6hR15wBhbV13WKy4ngoz4HO3VX_ujxTlVBbiZ9bLAmtBqCtkWQEiBmzmoQ';
 });
 
+app.db = db;
+app.hl_http_client = hl_http_client;
+
 couch_views.update_views(db, _);
 
 // Routes
@@ -56,8 +64,22 @@ app.get('/', function(req, res){
 });
 
 app.post('/apartments', function(req, res) {
-  db.saveDoc(null, req.body);
-  res.send(201);
+  var address = querystring.stringify({address: req.body.address + ', ' + req.body.post_code + ', Berlin, Germany', sensor: 'false'});
+  hl_http_client.get('maps.google.com', '/maps/api/geocode/json?' + address, function(err, body) {
+    if(err) {
+      send_error(res, err);
+    } else {
+      var location = JSON.parse(body).results[0].geometry.location;
+      var doc = _(req.body).extend({lat: location.lat, lng: location.lng, city: 'Berlin', country: 'Germany'});
+      db.saveDoc(doc, function(err, results) {
+        if(err) {
+          send_error(res, err);
+        } else {
+          res.send(201);
+        }
+      });
+    }
+  });
 });
 
 app.get('/apartments', function(req, res) {
@@ -65,7 +87,6 @@ app.get('/apartments', function(req, res) {
     res.send(results.rows);
   });
 });
-
 
 function send_error(res, er) {
   res.send(JSON.stringify(er), 500);
--- test/create_apartment_test.js
+++ test/create_apartment_test.js
@@ -1,19 +1,3 @@
-// // Run $ expresso
-// 
-// /**
-//  * Module dependencies.
-//  */
-// 
-// var app = require('../server');
-// var sys = require('sys');
-// 
-// module.exports = {
-//   'POST /apartments': function(assert){
-//     assert.response(app, {url: '/apartments'}, {status: 201});
-//   }
-// };
-
-
 // Run $ expresso
 
 /**
@@ -22,20 +6,38 @@
 
 var app = require('../server'),
   couchdb = app.db,
-  sys = require('sys');
+  hl_http_client = app.hl_http_client,
+  sys = require('sys'),
+  querystring = require('querystring');
 
 module.exports = {
   'POST /apartments': function(assert){
-    var doc;
-    
-    couchdb.saveDoc = function(docid, _doc) {
-      doc = _doc;
+    var doc, google_query;
+    couchdb.saveDoc = function(_doc, callback) {
+      doc = _doc; 
+      callback();
     };
+    
+    hl_http_client.get = function(host, query, callback) {
+      google_query = query;
+      callback(null, JSON.stringify(
+        {
+          "results": [ {
+            "geometry": {
+              "location": {
+                "lat": 37.4217080,
+                "lng": -122.0829964
+              }
+            }
+          } ]
+        }
+        ));
+    } 
     
     assert.response(app, {
       url: '/apartments',
       method: 'POST',
-      data: 'title=test+apartment',
+      data: 'title=test+apartment&address=broadway+5&post_code=10999',
       headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
@@ -44,10 +46,11 @@ module.exports = {
         status: 201
       },
       function(res) {
-        assert.eql(doc, {title: 'test apartment'});
+        assert.equal(querystring.unescape(google_query), '/maps/api/geocode/json?address=broadway 5, 10999, Berlin, Germany&sensor=false')
+        assert.eql(doc, {title: 'test apartment', address: 'broadway 5', city: 'Berlin', country: 'Germany', post_code: '10999', lat: 37.4217080, lng: -122.0829964});
       });
   },
-  'GET /apartments': function(assert){
+  'GET /apartments': function(assert) {
     couchdb.view = function(design, view, callback) {
       callback(null, {
         rows: [
