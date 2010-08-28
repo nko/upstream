f85379d1401eb70ef9cb26f9d7ce6f0564e88b6f
endor <fp@notjusthosting.com>
2010-08-28 20:24:28 +0200

    clean up filters

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,31 +1,38 @@
 $(function() {
-
   $(window).resize(function() {
     $('#map').height($(window).height() - $("#header").height() - $("#footer").height() - $("#bookmarks").height());
   });
   $(window).trigger("resize");
 
-  var amount_slider = $("#slider-range");
-	amount_slider.slider({
+  var price_slider = $("#price_range");
+	price_slider.slider({
 		range: true,
 		min: 10,
 		max: 2000,
 		step: 50,
 		values: [10, 2000],
 		slide: function(event, ui) {
-			$("#amount").val('$' + ui.values[0] + ' - $' + ui.values[1]);
-			
+			$("#price_amount").val('$' + ui.values[0] + ' - $' + ui.values[1]);			
 		}
 	});
 	
-	var min = amount_slider.slider("values", 0),
-	  max = amount_slider.slider("values", 1);
-	$("#amount").val('$' + min + ' - $' + max);
+	var space_slider = $('#space_range');
+	space_slider.slider({
+		range: true,
+		min: 10,
+		max: 2000,
+		step: 50,
+		values: [10, 2000],
+		slide: function(event, ui) {
+			$("#space_amount").val('$' + ui.values[0] + ' - $' + ui.values[1]);			
+		}
+	});
+	
+	var min = price_slider.slider("values", 0),
+	  max = price_slider.slider("values", 1);
+	$("#price_amount").val('$' + min + ' - $' + max);
 
-  $("form#ui input").filter(":checkbox,:radio").checkbox();
-
-
-
-
-  
+	var min = space_slider.slider("values", 0),
+	  max = space_slider.slider("values", 1);
+	$("#space_amount").val('$' + min + ' - $' + max);
 });
