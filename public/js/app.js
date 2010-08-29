68a44bfa2a1b92febcba8e71943e7e1511e04041
Kristina Schneider <mail@kristinaschneider.de>
2010-08-29 15:22:51 +0200

    style add ap. form

--- public/js/app.js
+++ public/js/app.js
@@ -143,4 +143,28 @@ $(function() {
       });
     }
   }
+  
+  // fix for HTML5 placeholder
+  $('[placeholder]').focus(function() {
+    var input = $(this);
+    if (input.val() == input.attr('placeholder')) {
+      input.val('');
+      input.removeClass('placeholder');
+    }
+  }).blur(function() {
+    var input = $(this);
+    if (input.val() == '') {
+      input.addClass('placeholder');
+      input.val(input.attr('placeholder'));
+    }
+  }).blur();
+  
+  $('[placeholder]').parents('form').submit(function() {
+    $(this).find('[placeholder]').each(function() {
+      var input = $(this);
+      if (input.val() == input.attr('placeholder')) {
+        input.val('');
+      }
+    })
+  });
 });
