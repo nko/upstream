0f6677cc5acabcbaed59d536d1070dfa94913bfe
endor <fp@notjusthosting.com>
2010-08-29 13:10:42 +0200

    resize and move filters and details container depending on the map size

--- public/js/app.js
+++ public/js/app.js
@@ -86,10 +86,25 @@ $(function() {
     });
   };
   
+  $(window).resize(function() {
+    var map = $('#map'),
+      filters = $('#filters'),
+      details_container = $('#details_container'),
+      map_height = $(window).height() - $("#header").height() - $("#footer").height() - $("#bookmarks").height(),
+      filter_height = filters.height();
+      
+    map.height(map_height);
+    filters.css('margin-top', (map_height - parseInt(filters.css('height'), 10)) / 2);
+    details_container.css('margin-top', 24).css('height', map_height - 48);
+  });
+  $(window).trigger("resize");
+  
+  var filters = $('#filters');
+  filters.find('.view_indicator').css('height', filters.find('ul').css('height'));
+  
   var details_container = $('#details_container');
   var width = details_container.css('width');
   details_container.css('right', '-' + width);
-  
   w4lls.show_details = function(apartment) {
     var show_big_details = function() {
       var filters = $('#filters'),
