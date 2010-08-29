35d63f9a64147c033910aa0e4c7b0cf1a8c6c869
Alexander Lang <alex@upstream-berlin.com>
2010-08-29 21:03:58 +0200

    deep links for apartments

--- public/js/app.js
+++ public/js/app.js
@@ -153,12 +153,11 @@ $(function() {
     
     w4lls.template('show', 'bookmarks', function() {
       $('#bookmarks ul').append(Mustache.to_html(w4lls.show_template, apartment));
+      
       $('#bookmarks ul .bookmark:last a.details').click(function() {
-        w4lls.show_details(apartment, function(apartment) {
-          $('.remember_this').click(function(evt) { w4lls.add_bookmark(apartment); evt.stopPropagation(); evt.preventDefault(); });
-          $('.send_request').click(function(evt) { w4lls.send_request(apartment); evt.stopPropagation(); evt.preventDefault(); });
-        });
+        location.hash = '/apartments/' + apartment._id;
       });
+      
       $('#bookmarks ul .bookmark:last a.delete_bookmark').click(function() {
         w4lls.delete_bookmark(apartment, this);
       });
@@ -201,6 +200,7 @@ $(function() {
       details_container.animate({right: '-3px'});
 
       w4lls.close_details_container = function() {
+        location.hash = '';
         w4lls.show_filters(filters);
         details_container.animate({right: '-' + width});
       }
